e5845d7c75f77356586ce9623b29c3e59e9af9fb
FatimaabdulrahmanBaris <95968497+FatimaabdulrahmanBaris@users.noreply.github.com>
2023-08-26 14:22:05 +0300

    Add files via upload

---
+++ test javascript/js/question.js
@@ -0,0 +1,31 @@
+
+//Array of Objects
+
+const quiz = [
+    {
+        q:'Which month comes right before june?',
+        options:['may','september','july','august'],
+        answer:0
+
+    },
+    {
+        q:'What color is a banana?',
+        options:['red','yellow','white','blue'],
+        answer:1
+    },
+    {
+        q:'3 + 4 = 7 ?',
+        options:['true','false'],
+        answer:0
+    },
+    {
+        q:'what time of day do we hav breakfast',
+        options:['In the afternoon','In the evening','In the morning'],
+        answer:2
+    },
+    {
+        q:'what is 22 + 6?',
+        options:['99','56','16','28'],
+        answer:3
+    },
+]
